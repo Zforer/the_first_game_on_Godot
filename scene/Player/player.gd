ef0be2390b4b61d6bfdabfa476973f6c150124cf
Zforer <you@example.com>
2026-02-16 10:56:05 +0500

    edited_sprites

--- scene/Player/player.gd
+++ scene/Player/player.gd
@@ -2,14 +2,22 @@ extends CharacterBody2D
 
 const max_speed = 125
 const acceleration = 0.15
-
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _process(delta):
 	var movement = movement_vector()
 	var direction = movement.normalized()
 	velocity = max_speed * direction
 	move_and_slide()
-
+	
+	if Input.is_action_pressed("move_right"):
+		_animated_sprite.play("run_right")
+	elif Input.is_action_pressed("move_left"):
+		_animated_sprite.play("run_left")
+	#elif Input.is_action_pressed("move_ap"):
+		#_animated_sprite.play("run_ap")
+	else:
+		_animated_sprite.play("idle")
 
 func movement_vector():
 	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
--- scene/pumpkin.gd
+++ scene/pumpkin.gd
@@ -2,19 +2,33 @@ extends CharacterBody2D
 
 var max_speed = 50
 
-
 func _process(delta):
-	var direction = get_direction__to_player()
-	velocity = max_speed * direction
+	var direction = get_direction_to_player()
+	
+	if direction != Vector2.ZERO:
+		velocity = max_speed * direction
+		if direction.x > 0:
+			play_animation("run_right")
+		elif direction.x < 0:
+			play_animation("run_left")
+	else:
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
 
 
-func get_direction__to_player():
-	var player = get_tree().get_first_node_in_group('player') as Node2D
-	if player != null:
+func get_direction_to_player() -> Vector2:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player:
 		return (player.global_position - global_position).normalized()
 	else:
 		return Vector2.ZERO
+
+
+func play_animation(anim_name):
+	var anim_sprite = $AnimatedSprite2D
+	if anim_sprite and anim_sprite.animation != anim_name:
+		anim_sprite.play(anim_name)
 
 
 func _on_area_2d_area_entered(area):
